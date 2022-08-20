dafef1c4dbd27b712ddb55592d4fd42a617bbcd2
huyouzhi1 <huyouzhi1@cmcm.com>
2022-08-21 00:31:29 +0800

    feat: del unused code

--- cmd/gs/repair.go
+++ cmd/gs/repair.go
@@ -32,10 +32,6 @@ func init() {
 
 // repair 修复远程项目的链接
 func repair(rootDir string, projectName string, branch string) {
-	// branch := "main"
-	// if len(os.Args) > 3 {
-	// 	branch = os.Args[3]
-	// }
 	_, dir, project := validProject(projectName)
 	internal.Add(rootDir, project, dir, branch)
 }
--- cmd/gs/root.go
+++ cmd/gs/root.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path"
 	"regexp"
 	"strings"
 
@@ -26,14 +25,6 @@ func validProject(project string) (prefix string, dir string, _ string) {
 	return prefix, fmt.Sprintf("%s/%s", prefix, project), project
 }
 
-// arg 获取命令行参数
-func arg(index int) string {
-	if len(os.Args) > index {
-		return os.Args[index]
-	}
-	panic("not enough arg")
-}
-
 var rootCmd = &cobra.Command{
 	Use:   "gs",
 	Short: "gs - a simple CLI to transform and inspect strings",
@@ -41,74 +32,12 @@ var rootCmd = &cobra.Command{
    
 One can use gs to add or modfiy go spring project from the terminal`,
 	Run: func(cmd *cobra.Command, args []string) {
-
-		// todo
 	},
 }
 
 func init() {
 	log.SetFlags(log.Lshortfile)
 	rootCmd.PersistentFlags().BoolP("backup", "b", true, "backup project code.")
-	// rootCmd.CompletionOptions.DisableNoDescFlag = true
-	// rootCmd.CompletionOptions.DisableDescriptions = true
-}
-
-func prepare() {
-	// fmt.Println(help)
-	// defer func() { fmt.Println() }()
-
-	// defer func() {
-	// 	if r := recover(); r != nil {
-	// 		log.Println(r)
-	// 		os.Exit(-1)
-	// 	}
-	// }()
-
-	// command := arg(1)
-	// cmd, ok := commands[command]
-	// if !ok {
-	// 	panic("error command " + command)
-	// }
-
-	// 获取工作目录
-	rootDir, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
-	// 加载 project.xml 配置文件
-	projectFile := path.Join(rootDir, "project.xml")
-	err = projectXml.Read(projectFile)
-	if err != nil {
-		panic(err)
-	}
-
-	count := len(projectXml.Projects)
-	defer func() {
-		if count != len(projectXml.Projects) {
-			// 保存 project.xml 配置文件
-			err = projectXml.Save(projectFile)
-			if err != nil {
-				panic(err)
-			}
-		}
-	}()
-
-	// fmt.Print(os.Args, " 输入 Yes 执行该命令: ")
-	// input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	// if strings.TrimSpace(input) != "Yes" {
-	// 	os.Exit(-1)
-	// }
-
-	// 备份本地文件
-	// if cmd.backup {
-	// 	internal.Zip(rootDir)
-	// }
-
-	// 执行命令
-	// if cmd.fn != nil {
-	// 	cmd.fn(rootDir)
-	// }
 }
 
 func Execute() {
